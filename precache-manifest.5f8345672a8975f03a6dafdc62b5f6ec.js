9924dc5f009d51da4f55d3e13c1192eb9859b1ac
Anjani <pranav.ch.12@outlook.com>
2022-05-03 19:32:30 +0200

    Updates

---
+++ precache-manifest.5f8345672a8975f03a6dafdc62b5f6ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bac72f68644c1adf7c58",
+    "url": "/YFF_RegEx/static/js/main.bac72f68.chunk.js"
+  },
+  {
+    "revision": "2ce5f974528cb6adb6e3",
+    "url": "/YFF_RegEx/static/css/1.4b8796da.chunk.css"
+  },
+  {
+    "revision": "2ce5f974528cb6adb6e3",
+    "url": "/YFF_RegEx/static/js/1.2ce5f974.chunk.js"
+  },
+  {
+    "revision": "57c62082c59bbb3fdf48",
+    "url": "/YFF_RegEx/static/js/runtime~main.57c62082.js"
+  },
+  {
+    "revision": "be0d20cf54f0685307146143d3fea305",
+    "url": "/YFF_RegEx/index.html"
+  }
+];
